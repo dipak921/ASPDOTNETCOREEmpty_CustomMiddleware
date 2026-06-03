7f11693c120eb94ab414e3a0cf8af5484f1ff7af
dipak921 <dipakchande921@gmail.com>
2026-06-03 11:40:29 +0530

    Today lern viewData and viewBag

--- ASPDOTNETCOREEmpty_CustomMiddleware/Controllers/HomeController.cs
+++ ASPDOTNETCOREEmpty_CustomMiddleware/Controllers/HomeController.cs
@@ -6,6 +6,28 @@ namespace ASPDOTNETCOREEmpty_CustomMiddleware.Controllers
     {
         public IActionResult Index()
         {
+            //ViewData["Message"] = "Welcome to ASP.NET Core MVC!";
+            //ViewData["CurrentTime"] = DateTime.Now.ToString("F");
+            //ViewData["Age"] = 30;
+
+            //string[] arr = { "ASP.NET Core", "MVC", "C#", "Programming"
+            //};
+            //ViewData["Array"] = arr;
+
+            //List<string> list = new List<string>()
+            //{
+            //    "Dipak", "Suman", "Rohit", "Satyarth"
+            //};
+            //ViewData["List"] = list;
+
+
+            // ********** view Bag ***********
+            ViewBag.Message = "Welcome to View Bag!";
+            ViewBag.CurrentTime = DateTime.Now.ToString("F");
+            ViewBag.Age = 30;
+
+            ViewBag.Array = new string[] { "ASP.NET Core", "MVC", "C#", "Programming" };
+
             return View();
         }
 
